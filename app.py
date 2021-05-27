48cb98377dc80d0d3a1936a020d44bc5919148be
shweta61999 <shwetapanhalkar4@gmail.com>
2021-05-27 21:22:38 +0530

    home.html added app.py updated

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 # print("Hello World")
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        print(request)
+        data = []   #empty dict to store data
+        data = request.json['team1']
+        print(data)
+
+        # return jsonify(data)
+    else:
+        return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
